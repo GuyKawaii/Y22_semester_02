c729b084370c4b1e883f42c755d654234ac0bc2f
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2022-08-31 16:15:04 +0200

    non git-versioned sub folder

---
+++ SpringAddTwoNew/src/main/java/com/example/springaddtwonew/SpringAddTwoNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaddtwonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAddTwoNewApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAddTwoNewApplication.class, args);
+  }
+  
+}
---
+++ SpringAddTwoNew/src/test/java/com/example/springaddtwonew/SpringAddTwoNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaddtwonew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAddTwoNewApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
